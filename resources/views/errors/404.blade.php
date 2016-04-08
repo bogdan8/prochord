538cd3f6d5afe6ca57ea5f704d53617285eaefba
bogdan8 <bogdanlitvin8@gmail.com>
2016-04-08 17:37:03 +0300

    change block with share social chain

--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -1,51 +1,52 @@
 <!DOCTYPE html>
 <html lang="uk">
-    <head>
-        <meta charset="utf-8">
-        <title>Be right back.</title>
+<head>
+    <meta charset="utf-8">
+    <title>Be right back.</title>
 
-        <link href="https://fonts.googleapis.com/css?family=Lato:100" rel="stylesheet" type="text/css">
+    <link href="https://fonts.googleapis.com/css?family=Lato:100" rel="stylesheet" type="text/css">
 
-        <style>
-            html, body {
-                height: 98%;
-            }
+    <style>
+        html, body {
+            height: 98%;
+        }
 
-            body {
-                background: url(/image/errors/fon_error.jpg)  center center fixed;
-                background-size: 100%;
-                margin: 0;
-                padding: 0;
-                width: 100%;
-                color: #B0BEC5;
-                display: table;
-                font-weight: 100;
-                font-family: 'Lato';
-            }
+        body {
+            background: url(/image/errors/404/fon_error.jpg) center center fixed;
+            background-size: 100%;
+            margin: 0;
+            padding: 0;
+            width: 100%;
+            color: #B0BEC5;
+            display: table;
+            font-weight: 100;
+            font-family: 'Lato';
+            overflow: hidden;
+        }
 
-            .container {
-                text-align: center;
-                display: table-cell;
-                vertical-align: middle;
-            }
+        .container {
+            text-align: center;
+            display: table-cell;
+            vertical-align: middle;
+        }
 
-            .content {
-                text-align: center;
-                display: inline-block;
-            }
+        .content {
+            text-align: center;
+            display: inline-block;
+        }
 
-            .title {
-                font-size: 40px;
-                color: #000000;
-            }
-        </style>
-    </head>
-    <body>
-        <div class="container">
-            <div class="content">
-                <div class="title">404 Сторінку незнайдено</div>
-                <div class="title">Нажміть<a href="/"> тут </a> щоб повернутись на головну!</div>
-            </div>
-        </div>
-    </body>
+        .title {
+            font-size: 40px;
+            color: #000000;
+        }
+    </style>
+</head>
+<body>
+<div class="container">
+    <div class="content">
+        <div class="title">404 Сторінку незнайдено</div>
+        <div class="title">Нажміть<a href="/"> тут </a> щоб повернутись на головну!</div>
+    </div>
+</div>
+</body>
 </html>
--- resources/views/song/cartSong.blade.php
+++ resources/views/song/cartSong.blade.php
@@ -92,7 +92,7 @@
 
                             <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
                                 <div class="row">
-                                    <div class="col-lg-6 col-md-6 col-sm-6 col-xs-12">
+                                    <div class="col-lg-3 col-md-3 col-sm-3 col-xs-12">
                                         @if(!empty($cartSong->performer_id))
                                             <p>
                                                 {{trans('translation.Виконавець')}} :
@@ -118,7 +118,7 @@
 
                                         @endif
                                     </div>
-                                    <div class="col-lg-6 col-md-6 col-sm-6 col-xs-12">
+                                    <div class="col-lg-4 col-md-4 col-sm-4 col-xs-12">
                                         @if(!empty($cartSong->tabulature))
                                             <p>
                                                 {{trans('translation.Як_грати')}} :
@@ -131,12 +131,38 @@
                                                 }
                                                 echo $tabulature;
                                                 ?>
-                                                ?>
                                             </p>
                                         @endif()
                                     </div>
+                                    <div class="col-lg-5 col-md-5 col-sm-5 col-xs-12">
+                                        <!-- script to share the song in social networks-->
+                                        <script type="text/javascript">(function () {
+                                                if (window.pluso)if (typeof window.pluso.start == "function") return;
+                                                if (window.ifpluso == undefined) {
+                                                    window.ifpluso = 1;
+                                                    var d = document, s = d.createElement('script'), g = 'getElementsByTagName';
+                                                    s.type = 'text/javascript';
+                                                    s.charset = 'UTF-8';
+                                                    s.async = true;
+                                                    s.src = ('https:' == window.location.protocol ? 'https' : 'http') + '://share.pluso.ru/pluso-like.js';
+                                                    var h = d[g]('body')[0];
+                                                    h.appendChild(s);
+                                                }
+                                            })();
+                                        </script>
+                                        <div data-description="{{ strtr($cartSong->body,"_"," ")}}" style="margin-left: 20px; "
+                                             data-title="{{$cartSong->title}}"
+                                             data-url="http://test.ifka.kr.ua/songs/{{$cartSong->title}}" class="pluso"
+                                             data-background="transparent"
+                                             data-options="medium,square,multiline,horizontal,counter,theme=04"
+                                             data-services="vkontakte,odnoklassniki,facebook,twitter,google,moimir,email,print">
+
+                                        </div>
+                                        <!-- End script to share the song in social networks-->
+                                    </div>
                                 </div>
                                 <!-- text settings -->
+                                <br />
                                 <div class="row">
                                     <div class="col-lg-2 col-md-2 col-sm-2 col-xs-12">
                                         <h4 style="color: #1B602C;">{{trans('translation.Шрифт')}} :</h4>
@@ -219,30 +245,6 @@
                             @endif
                         </div>
                     </div>
-                    <!-- script to share the song in social networks-->
-                    <script type="text/javascript">(function () {
-                            if (window.pluso)if (typeof window.pluso.start == "function") return;
-                            if (window.ifpluso == undefined) {
-                                window.ifpluso = 1;
-                                var d = document, s = d.createElement('script'), g = 'getElementsByTagName';
-                                s.type = 'text/javascript';
-                                s.charset = 'UTF-8';
-                                s.async = true;
-                                s.src = ('https:' == window.location.protocol ? 'https' : 'http') + '://share.pluso.ru/pluso-like.js';
-                                var h = d[g]('body')[0];
-                                h.appendChild(s);
-                            }
-                        })();
-                    </script>
-                    <div data-description="{{ strtr($cartSong->body,"_"," ")}}" style="margin-left: 20px; "
-                         data-title="{{$cartSong->title}}"
-                         data-url="http://test.ifka.kr.ua/songs/{{$cartSong->title}}" class="pluso"
-                         data-background="transparent"
-                         data-options="medium,square,multiline,horizontal,counter,theme=04"
-                         data-services="vkontakte,odnoklassniki,facebook,twitter,google,moimir,email,print">
-
-                    </div>
-                    <!-- End script to share the song in social networks-->
                     @if($cartSong->who_added != 'admin')
                         <h3 class="who-added-song">{{trans('translation.Дякуємо').' `'.$cartSong->who_added.'`'}} {{trans('translation.за_добавлену_пісню')}}</h3>
                     @endif
