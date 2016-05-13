abc06a712a6935c660cb87ba050e131c86e886f5
bogdan8 <bogdanlitvin8@gmail.com>
2016-05-13 13:48:45 +0300

    bag fix paginate

--- resources/views/song/ajaxPaginate/SongsInCategory.blade.php
+++ resources/views/song/ajaxPaginate/SongsInCategory.blade.php
@@ -9,15 +9,15 @@
             <p class="phone-title" style="display: none; float: left;">
                 {!! mb_substr(strip_tags($item->title),0 , 13)!!}...
             </p>
-                <span class="songIconCount">
-                    <i class="fa fa-eye"></i>
-                    {{$item->count_views_song}}
-                    <i class="fa fa-comments-o"></i>
-                    {!! count($item->songComment) !!}
-                    @if(!empty($item->video))
-                        <i class="fa fa-youtube-play"></i>
-                    @endif
-                </span>
+            <span class="songIconCount">
+                <i class="fa fa-eye"></i>
+                {{$item->count_views_song}}
+                <i class="fa fa-comments-o"></i>
+                {!! count($item->songComment) !!}
+                @if(!empty($item->video))
+                    <i class="fa fa-youtube-play"></i>
+                @endif
+            </span>
         </a>
     @endforeach
 </div>
